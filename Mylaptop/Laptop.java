e7905477d8cc8786360efe8865977206cd9a4a6d
alfiansyah hidayat <105407023+alfiansyahhidayat@users.noreply.github.com>
2022-11-12 20:11:54 +0700

    Add files via upload

---
+++ Mylaptop/Laptop.java
@@ -0,0 +1,11 @@
+package OOPsesi6.Mylaptop;
+
+public interface Laptop {
+    int MAX_VOL=100;
+    int MIN_VOL=0;
+
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
